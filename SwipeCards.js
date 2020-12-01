3405d856a652147df6a1af2197055e352dbd5c15
branko993 <branko993@gmail.com>
2020-12-01 21:45:15 +0100

    added unsafe component will receive props

--- SwipeCards.js
+++ SwipeCards.js
@@ -343,7 +343,7 @@ export default class SwipeCards extends Component {
     }).start();
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     if (nextProps.cards !== this.props.cards) {
       if (this.cardAnimation) {
         this.cardAnimation.stop();
